4aa2bac8d1ee60a55fde795deb6b0517133d2d73
Shaabna Sara <shaabnasara21@gmail.com>
2023-12-19 17:43:35 +0200

    JS_TASK1

---
+++ main.js
@@ -0,0 +1,6 @@
+// Part 1
+document.querySelector("div").classList.add("demo");
+
+// Part 2
+document.querySelector("h1").classList.add("vip");
+document.querySelector("h1").classList.remove("vip");
